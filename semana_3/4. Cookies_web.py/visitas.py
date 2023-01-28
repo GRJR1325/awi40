cf09b0dfd3c581e11404f9d58b093afa1e5f92bd
GRJR_1325 <97265804+GRJR1325@users.noreply.github.com>
2023-01-28 05:02:25 +0000

    Ultimate changes

--- semana_3/4. Cookies_web.py/visitas.py
+++ semana_3/4. Cookies_web.py/visitas.py
@@ -1,25 +1,31 @@
-import web
+import web #WEB PY
+import time #IMPORTAMOS EL TIEMPO
+
 
 class Visitas:
-    def GET(self, nombre):
+    def GET(self, nombre):  
         try:
             cookie = web.cookies()
-            visitas = 0 
+            dia_ingreso = time.strftime("%d/%m/%y") #DIA
+            hora_ingreso = time.strftime("%I:%M:%S") #HORA
+            visitas = 0 #VISITAS
             print(cookie)
-            if nombre:
+            if nombre: #SI HAY UN NOMBRE LO IMPRIMIRA
                 web.setcookie("nombre",nombre,expires="",domain=None)
             else:
-                nombre = "Anonimo"
+                nombre = "Anonimo" #DEL CASO CONTRARIO SERA ANONIMO
                 web.setcookie("nombre",nombre,expires="",domain=None)
             if cookie.get("visitas"):
                 visitas = int(cookie.get("visitas"))
                 visitas += 1 #SUMA UNA VISITA 
                 web.setcookie("visitas", str(visitas), expires="", domain=None) 
-                #REGRESA LA CANTIDAD DE VISIAS
             else:
                 web.setcookie("visitas", str(1), expires="", domain=None)
                 visitas = "1"
                 
-            return "visitas:" + str(visitas) + " Nombre: "  + nombre
+            return ("No. visitas:" + str(visitas) + " | "+ 
+            " Usuario: "  + nombre + " | " + 
+            " Dia de ingreso:" + str(dia_ingreso) + " | " + 
+            " Hora de Ingreso:" + str(hora_ingreso)) 
         except Exception as e:
             return "Error " + str(e.args)
